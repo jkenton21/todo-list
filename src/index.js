4b2c98da98ee9c5980c5013f09ab278c4611336d
jkenton21 <jkenton21@gmail.com>
2022-03-21 18:04:15 -0500

    basic webpage layout added with header/footer and styling

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// JavaScript Index file for Todo List project
+// The main purpose of this script is to import modules that will be packaged
+// up using webpack to run the html. The html will pull from the packaged
+// file "main.js" in the dist/ folder in th project directory.
